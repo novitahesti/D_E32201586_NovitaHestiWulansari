1cc893aae67abeb57ff328efeededc5e07bae645
novitahesti <novitahesti174@gmail.com>
2021-04-22 09:02:42 +0700

    tugas minggu 7

---
+++ Minggu 7/routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\frontend;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Backend\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/belajar', function () {
+    echo '<h1>Hello World</h1>';
+    echo '<p>Sedang belajar Laravel</p>';
+});
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+Route::get('page/{nomor}', function($nomor){
+	Return 'Ini Halaman ke-' . $nomor;
+});
+Route::get('/gambar', function () {
+    return view('gambar');
+});
+
+Route::get('user', [NovitahesController::class, 'index']);
+Route::resource('user', NovitahesController::class);
+
+Route::get("/novita", function(){
+    return view("novita");
+});
+
+//Route::group(['namespace' => 'Frontend'], function() {
+Route::resource('home', HomeController::class);
+
+//Route::group(['namespace' => 'Backend'], function() {
+Route::resource('dashboard', DashboardController::class);
+
+
+Route::resource('home', HomeController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['web', 'auth'])->group(function () {
+    Route::resource('dashboard', DashboardController::class);
+});
